1b15034a497d7faad959e73804309cf79545cbee
Valoa <olav.huizer@gmail.com>
2022-11-09 15:07:53 +0100

    some more animations..

--- 11November/11November.ino
+++ 11November/11November.ino
@@ -9,47 +9,48 @@
 
 */
 
-//libs --> fastled  https://github.com/FastLED/FastLED 
-// animations taken from: https://www.tweaking4all.com/hardware/arduino/adruino-led-strip-effects with some changes to account for the RGBW part of the thing. 
+//libs --> fastled  https://github.com/FastLED/FastLED
+// animations taken from: https://www.tweaking4all.com/hardware/arduino/adruino-led-strip-effects with some changes to account for the RGBW part of the thing.
 
 
 // 11 led on one side rgbw D2
 //one button on  D1
 
 #include <FastLED.h>
-#include "FastLED_RGBW.h"
+#include "FastLED_RGBW.h" // litle hack from  (http://krazydad.com). 
 
-const int BUTTON = 2; // button pin 
-const int aniAmount = 4; // amount of animations that are running in the loop (aray starts at 0 so its 5 now )
-int animationSelector = 0; // little animationSelector 
-int moveSpeed = 40; // used as a delay in the on led travel animation 
+const int BUTTON = 2; // button pin
+const int aniAmount = 8; // amount of animations that are running in the loop (aray starts at 0 so its 9 now )
+int animationSelector = 0; // little animationSelector
+int moveSpeed = 40; // used as a delay in the on led travel animation
 
 //local LEDs
-#define NUM_LEDS 11
+#define NUM_LEDS 11 // amount of leds 
 #define LED_PIN 4  // led pin
 
-CRGBW leds[NUM_LEDS]; // hack to get rgbw working, im sub buffering the leds and inserting an extra channel 
-CRGB *ledsRGB = (CRGB *) &leds[0]; // leds 
+CRGBW leds[NUM_LEDS]; // hack to get rgbw working, im sub buffering the leds and inserting an extra channel
+CRGB *ledsRGB = (CRGB *) &leds[0]; // leds
 
-int ledOn = 1; // variable used for the single led chace 
+int ledOn = 1; // variable used for the single led chace
 
 
 void setup() {
+  
   //start serial port
   Serial.begin(115200);
   Serial.print("11 novemember is de dag");
 
-  pinMode(LED_PIN, OUTPUT);     //set the led pin as output 
-  pinMode(BUTTON, INPUT_PULLUP); // set the button as input pullup 
-  attachInterrupt(digitalPinToInterrupt(BUTTON), blink, FALLING  ); //set the button as interupt so when it is pressed it interupts the program 
-  FastLED.addLeds<WS2812B, LED_PIN, RGB>(ledsRGB, getRGBWsize(NUM_LEDS)); // add leds to the array 
-
-  initTest(); // show a little test rgb seqence 
+  pinMode(LED_PIN, OUTPUT);     //set the led pin as output
+  pinMode(BUTTON, INPUT_PULLUP); // set the button as input pullup
+  attachInterrupt(digitalPinToInterrupt(BUTTON), blink, FALLING  ); //set the button as interupt so when it is pressed it interupts the program
+  FastLED.addLeds<WS2812B, LED_PIN, RGB>(ledsRGB, getRGBWsize(NUM_LEDS)); // add leds to the array
+  animationSelector = random(aniAmount);
+  initTest(); // show a little test rgb seqence
 
 }
 
 void loop() {
-  
+
   // Serial.println(animationSelector);
   if  (animationSelector == 0) {
     Fire(55, 120, 15);
@@ -62,22 +63,30 @@ void loop() {
     OneLedTravel();
   } else  if  (animationSelector == 4) {
     Sparkle(0xff, 0xff, 0xff, 100);
+  } else if (animationSelector == 5) {
+    FadeInOut(255, 0, 240); // pink?
+  } else if (animationSelector == 6) {
+    FadeInOut(255, 100, 0); // yellow
+  } else  if  (animationSelector == 7) {
+    Sparkle(0, 127, 255, 100);
+  } else  if  (animationSelector == 8) {
+   //rainbowCycle(10);
+   CylonBounce(0xff, 0, 0, 4, 20, 50);// little kit animaation 
   }
-
 }
 
 
-void blink() { // interyupt fuction that increments the animation selector value 
+void blink() { // interyupt fuction that increments the animation selector value
   //animationSelector = random(aniAmount);
   animationSelector++;
-  if ( animationSelector > aniAmount) {  // circle round 
+  if ( animationSelector > aniAmount) {  // circle round
     animationSelector = 0;
   }
   //Serial.println(animationSelector);
 }
 
 
-void initTest() // little test animation 
+void initTest() // little test animation
 {
   for (int i = 0 ; i < NUM_LEDS ; i++)
   {
@@ -116,32 +125,32 @@ void initTest() // little test animation
 }
 
 
-void showStrip() { // used in the animaiton tab 
+void showStrip() { // used in the animaiton tab
   FastLED.show();
 }
 
-void setPixel(int Pixel, byte red, byte green, byte blue) { // used for animations 
+void setPixel(int Pixel, byte red, byte green, byte blue) { // used for animations
   leds[Pixel].r = red;
   leds[Pixel].g = green;
   leds[Pixel].b = blue;
   //CRGBW(0, 0, 0, 127);
 }
 
-void setAll(byte red, byte green, byte blue) { // used in animations 
-  for (int i = 0; i < NUM_LEDS; i++ ) { // loop thru all and set the pixel colors 
+void setAll(byte red, byte green, byte blue) { // used in animations
+  for (int i = 0; i < NUM_LEDS; i++ ) { // loop thru all and set the pixel colors
     setPixel(i, red, green, blue);
   }
-  showStrip(); // send data to the leds 
+  showStrip(); // send data to the leds
 }
 
-void OneLedTravel() { // one led travel animation 
- 
-  leds[ledOn % NUM_LEDS] = CRGBW(255, 255, 255, 255); // color of the led  // using modulo on the amount of leds to make it travel withoin bounds// will eventualy overflow not sure what happends then :) 
+void OneLedTravel() { // one led travel animation
+
+  leds[ledOn % NUM_LEDS] = CRGBW(255, 255, 255, 255); // color of the led  // using modulo on the amount of leds to make it travel withoin bounds// will eventualy overflow not sure what happends then :)
   //leds[ledOn] = CRGBW(0, 0, 0, 255);
   //leds[ledOn+1] = CRGBW(0, 0, 255, 0);
   FastLED.show();
-  delay(moveSpeed); 
-  leds[ledOn % NUM_LEDS] = CRGBW(0, 0, 0, 0); 
+  delay(moveSpeed);
+  leds[ledOn % NUM_LEDS] = CRGBW(0, 0, 0, 0);
   // leds[ledOn+1] = CRGBW(0, 0, 255, 0);
 
   ledOn++;
--- 11November/Animations.ino
+++ 11November/Animations.ino
@@ -1,3 +1,90 @@
+void FadeInOut(byte red, byte green, byte blue){
+  float r, g, b;
+     
+  for(int k = 0; k < 256; k=k+1) {
+    r = (k/256.0)*red;
+    g = (k/256.0)*green;
+    b = (k/256.0)*blue;
+    setAll(r,g,b);
+    showStrip();
+  }
+     
+  for(int k = 255; k >= 0; k=k-2) {
+    r = (k/256.0)*red;
+    g = (k/256.0)*green;
+    b = (k/256.0)*blue;
+    setAll(r,g,b);
+    showStrip();
+  }
+}
+
+void CylonBounce(byte red, byte green, byte blue, int EyeSize, int SpeedDelay, int ReturnDelay){
+
+  for(int i = 0; i < NUM_LEDS-EyeSize-2; i++) {
+    setAll(0,0,0);
+    setPixel(i, red/10, green/10, blue/10);
+    for(int j = 1; j <= EyeSize; j++) {
+      setPixel(i+j, red, green, blue);
+    }
+    setPixel(i+EyeSize+1, red/10, green/10, blue/10);
+    showStrip();
+    delay(SpeedDelay);
+  }
+
+  delay(ReturnDelay);
+
+  for(int i = NUM_LEDS-EyeSize-2; i > 0; i--) {
+    setAll(0,0,0);
+    setPixel(i, red/10, green/10, blue/10);
+    for(int j = 1; j <= EyeSize; j++) {
+      setPixel(i+j, red, green, blue);
+    }
+    setPixel(i+EyeSize+1, red/10, green/10, blue/10);
+    showStrip();
+    delay(SpeedDelay);
+  }
+ 
+  delay(ReturnDelay);
+}
+
+void rainbowCycle(int SpeedDelay) {
+  byte *c;
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< NUM_LEDS; i++) {
+      c=Wheel(((i * 256 / NUM_LEDS) + j) & 255);
+      setPixel(i, *c, *(c+1), *(c+2));
+    }
+    showStrip();
+    delay(SpeedDelay);
+  }
+}
+
+byte * Wheel(byte WheelPos) {
+  static byte c[3];
+ 
+  if(WheelPos < 85) {
+   c[0]=WheelPos * 3;
+   c[1]=255 - WheelPos * 3;
+   c[2]=0;
+  } else if(WheelPos < 170) {
+   WheelPos -= 85;
+   c[0]=255 - WheelPos * 3;
+   c[1]=0;
+   c[2]=WheelPos * 3;
+  } else {
+   WheelPos -= 170;
+   c[0]=0;
+   c[1]=WheelPos * 3;
+   c[2]=255 - WheelPos * 3;
+  }
+
+  return c;
+}
+
+
+
 void Sparkle(byte red, byte green, byte blue, int SpeedDelay) {  // little sparkle animation 
   int Pixel = random(NUM_LEDS);
   setPixel(Pixel,red,green,blue);
@@ -94,6 +181,11 @@ void meteorRain(byte red, byte green, byte blue, byte meteorSize, byte meteorTra
     delay(SpeedDelay);
   }
 }
+
+
+
+
+
 
 void fadeToBlack(int ledNo, byte fadeValue) { // little fuctnion to fade led to black... without using the fastled lib so its compatible with animations from the internets 
  
