f32527416deb5286e6a6fb2db1b9c449f299cfa0
Valoa <olav.huizer@gmail.com>
2022-11-09 13:41:08 +0100

    11 november is de dag

---
+++ 11November/11November.ino
@@ -0,0 +1,159 @@
+//11 noveember little lights For Jolien van jelle
+/* 22 sk2812 rgbw leds controlled on one data line in stacks of 11
+   one button
+   arduino nano with bypassed, voltage regulator
+   18650 battery
+
+    animation tab can hold random led colors that can be changed ugins the button
+    if you add more add the aniAmount to the amont of variables and add more else if statements in the loop
+
+*/
+
+//libs --> fastled  https://github.com/FastLED/FastLED 
+// animations taken from: https://www.tweaking4all.com/hardware/arduino/adruino-led-strip-effects with some changes to account for the RGBW part of the thing. 
+
+
+// 11 led on one side rgbw D2
+//one button on  D1
+
+#include <FastLED.h>
+#include "FastLED_RGBW.h"
+
+const int BUTTON = 2; // button pin 
+const int aniAmount = 4; // amount of animations that are running in the loop (aray starts at 0 so its 5 now )
+int animationSelector = 0; // little animationSelector 
+int moveSpeed = 40; // used as a delay in the on led travel animation 
+
+//local LEDs
+#define NUM_LEDS 11
+#define LED_PIN 4  // led pin
+
+CRGBW leds[NUM_LEDS]; // hack to get rgbw working, im sub buffering the leds and inserting an extra channel 
+CRGB *ledsRGB = (CRGB *) &leds[0]; // leds 
+
+int ledOn = 1; // variable used for the single led chace 
+
+
+void setup() {
+  //start serial port
+  Serial.begin(115200);
+  Serial.print("11 novemember is de dag");
+
+  pinMode(LED_PIN, OUTPUT);     //set the led pin as output 
+  pinMode(BUTTON, INPUT_PULLUP); // set the button as input pullup 
+  attachInterrupt(digitalPinToInterrupt(BUTTON), blink, FALLING  ); //set the button as interupt so when it is pressed it interupts the program 
+  FastLED.addLeds<WS2812B, LED_PIN, RGB>(ledsRGB, getRGBWsize(NUM_LEDS)); // add leds to the array 
+
+  initTest(); // show a little test rgb seqence 
+
+}
+
+void loop() {
+  
+  // Serial.println(animationSelector);
+  if  (animationSelector == 0) {
+    Fire(55, 120, 15);
+  } else  if  (animationSelector == 1) {
+    colorWipe(255, 0, 255, 50);
+    colorWipe(0x00, 0x00, 0x00, 50);
+  } else if  (animationSelector == 2) {
+    meteorRain(0xff, 0xff, 0xff, 10, 64, true, 30);
+  } else  if  (animationSelector == 3) {
+    OneLedTravel();
+  } else  if  (animationSelector == 4) {
+    Sparkle(0xff, 0xff, 0xff, 100);
+  }
+
+}
+
+
+void blink() { // interyupt fuction that increments the animation selector value 
+  //animationSelector = random(aniAmount);
+  animationSelector++;
+  if ( animationSelector > aniAmount) {  // circle round 
+    animationSelector = 0;
+  }
+  //Serial.println(animationSelector);
+}
+
+
+void initTest() // little test animation 
+{
+  for (int i = 0 ; i < NUM_LEDS ; i++)
+  {
+    //leds[i] = CRGBW(127, 0, 0, 0);
+    leds[i] = CRGB(127, 0, 0);
+  }
+  FastLED.show();
+  delay(500);
+  for (int i = 0 ; i < NUM_LEDS ; i++)
+  {
+    //leds[i] = CRGBW(0, 127, 0, 0);
+    leds[i] = CRGB(0, 127, 0);
+  }
+  FastLED.show();
+  delay(500);
+  for (int i = 0 ; i < NUM_LEDS ; i++)
+  {
+    // leds[i] = CRGBW(0, 0, 127, 0);
+    leds[i] = CRGB(0, 0, 127);
+  }
+  FastLED.show();
+  delay(500);
+  for (int i = 0 ; i < NUM_LEDS ; i++)
+  {
+    //leds[i] = CRGBW(0, 0, 0, 127);
+    leds[i] = CRGB(127, 127, 127);
+  }
+  FastLED.show();
+  delay(500);
+  for (int i = 0 ; i < NUM_LEDS ; i++)
+  {
+    // leds[i] = CRGBW(0, 0, 0, 0);
+    leds[i] = CRGB(0, 0, 0);
+  }
+  FastLED.show();
+}
+
+
+void showStrip() { // used in the animaiton tab 
+  FastLED.show();
+}
+
+void setPixel(int Pixel, byte red, byte green, byte blue) { // used for animations 
+  leds[Pixel].r = red;
+  leds[Pixel].g = green;
+  leds[Pixel].b = blue;
+  //CRGBW(0, 0, 0, 127);
+}
+
+void setAll(byte red, byte green, byte blue) { // used in animations 
+  for (int i = 0; i < NUM_LEDS; i++ ) { // loop thru all and set the pixel colors 
+    setPixel(i, red, green, blue);
+  }
+  showStrip(); // send data to the leds 
+}
+
+void OneLedTravel() { // one led travel animation 
+ 
+  leds[ledOn % NUM_LEDS] = CRGBW(255, 255, 255, 255); // color of the led  // using modulo on the amount of leds to make it travel withoin bounds// will eventualy overflow not sure what happends then :) 
+  //leds[ledOn] = CRGBW(0, 0, 0, 255);
+  //leds[ledOn+1] = CRGBW(0, 0, 255, 0);
+  FastLED.show();
+  delay(moveSpeed); 
+  leds[ledOn % NUM_LEDS] = CRGBW(0, 0, 0, 0); 
+  // leds[ledOn+1] = CRGBW(0, 0, 255, 0);
+
+  ledOn++;
+  //  Serial.print("hour:");
+  //  Serial.println(hour);
+  // Serial.print("led:");
+  //  Serial.println(ledOn);
+  //  Serial.print("ledModulo:");
+  //  Serial.println(ledOn%60);
+  FastLED.show();
+  //    if (ledOn>=60 ){
+  //      ledOn=1;
+  //       //delay(50);
+  //    }
+}
---
+++ 11November/Animations.ino
@@ -0,0 +1,119 @@
+void Sparkle(byte red, byte green, byte blue, int SpeedDelay) {  // little sparkle animation 
+  int Pixel = random(NUM_LEDS);
+  setPixel(Pixel,red,green,blue);
+  showStrip();
+  delay(SpeedDelay);
+  setPixel(Pixel,0,0,0);
+}
+
+
+void Fire(int Cooling, int Sparking, int SpeedDelay) { // fire animation 
+  static byte heat[NUM_LEDS];
+  int cooldown;
+ 
+  // Step 1.  Cool down every cell a little
+  for( int i = 0; i < NUM_LEDS; i++) {
+    cooldown = random(0, ((Cooling * 10) / NUM_LEDS) + 2);
+   
+    if(cooldown>heat[i]) {
+      heat[i]=0;
+    } else {
+      heat[i]=heat[i]-cooldown;
+    }
+  }
+ 
+  // Step 2.  Heat from each cell drifts 'up' and diffuses a little
+  for( int k= NUM_LEDS - 1; k >= 2; k--) {
+    heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2]) / 3;
+  }
+   
+  // Step 3.  Randomly ignite new 'sparks' near the bottom
+  if( random(255) < Sparking ) {
+    int y = random(7);
+    heat[y] = heat[y] + random(160,255);
+    //heat[y] = random(160,255);
+  }
+
+  // Step 4.  Convert heat to LED colors
+  for( int j = 0; j < NUM_LEDS; j++) {
+    setPixelHeatColor(j, heat[j] );
+  }
+
+  showStrip();
+  delay(SpeedDelay);
+}
+
+
+void setPixelHeatColor (int Pixel, byte temperature) { 
+  // Scale 'heat' down from 0-255 to 0-191
+  byte t192 = round((temperature/255.0)*191);
+ 
+  // calculate ramp up from
+  byte heatramp = t192 & 0x3F; // 0..63
+  heatramp <<= 2; // scale up to 0..252
+ 
+  // figure out which third of the spectrum we're in:
+  if( t192 > 0x80) {                     // hottest
+    setPixel(Pixel, 255, 255, heatramp);
+  } else if( t192 > 0x40 ) {             // middle
+    setPixel(Pixel, 255, heatramp, 0);
+  } else {                               // coolest
+    setPixel(Pixel, heatramp, 0, 0);
+  }
+}
+
+void colorWipe(byte red, byte green, byte blue, int SpeedDelay) { // wipes the stip in to a solid color// add multiple behind each other to create cool effects on color chace 
+  for(uint16_t i=0; i<NUM_LEDS; i++) { 
+      setPixel(i, red, green, blue);
+      showStrip();
+      delay(SpeedDelay);
+  }
+}
+
+void meteorRain(byte red, byte green, byte blue, byte meteorSize, byte meteorTrailDecay, boolean meteorRandomDecay, int SpeedDelay) {   
+  setAll(0,0,0);
+ 
+  for(int i = 0; i < NUM_LEDS+NUM_LEDS; i++) {
+   
+   
+    // fade brightness all LEDs one step
+    for(int j=0; j<NUM_LEDS; j++) {
+      if( (!meteorRandomDecay) || (random(10)>5) ) {
+        fadeToBlack(j, meteorTrailDecay );        
+      }
+    }
+   
+    // draw meteor
+    for(int j = 0; j < meteorSize; j++) {
+      if( ( i-j <NUM_LEDS) && (i-j>=0) ) {
+        setPixel(i-j, red, green, blue);
+      }
+    }
+   
+    showStrip();
+    delay(SpeedDelay);
+  }
+}
+
+void fadeToBlack(int ledNo, byte fadeValue) { // little fuctnion to fade led to black... without using the fastled lib so its compatible with animations from the internets 
+ 
+    // NeoPixel
+    uint32_t oldColor;
+    uint8_t r, g, b;
+    int value;
+   
+    //oldColor = FastLED.getPixelColor(ledNo);
+    r = (oldColor & 0x00ff0000UL) >> 16;
+    g = (oldColor & 0x0000ff00UL) >> 8;
+    b = (oldColor & 0x000000ffUL);
+
+    r=(r<=10)? 0 : (int) r-(r*fadeValue/256);
+    g=(g<=10)? 0 : (int) g-(g*fadeValue/256);
+    b=(b<=10)? 0 : (int) b-(b*fadeValue/256);
+   
+    //strip.setPixelColor(ledNo, r,g,b);
+    leds[ledNo] = CRGB(r, g, b);
+   // FastLED
+   //leds[ledNo].fadeToBlackBy( fadeValue );
+ 
+}
